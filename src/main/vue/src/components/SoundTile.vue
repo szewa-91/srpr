67afa8528de205f07bbfca3022cce71e58689438
qwrtln <qwrtln@users.noreply.github.com>
2019-01-22 21:32:56 +0100

    Sound tiles (#3)
    Add images grid

---
+++ src/main/vue/src/components/SoundTile.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="sound-content">
+      <img :src="require(`@/${imgPath}`)" class="bg-img">
+      <h2>{{ name }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SoundTile extends Vue {
+
+  @Prop() private name!: string;
+  @Prop() private imgPath!: string;
+
+}
+</script>
+
+<style lang="scss">
+.sound-content {
+  width: 32vw;
+  height: 33vh;
+  position: relative;
+  vertical-align: middle;
+  text-align: center;
+}
+.bg-img {
+  max-width: 100%;
+  max-height: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+  z-index: -1;
+  opacity: .8;
+  object-fit: cover;
+  overflow: hidden;
+}
+</style>
