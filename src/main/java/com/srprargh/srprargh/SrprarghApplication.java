217ff81534de752eb6afd04c14402109c3372c7c
Szewa <marcin91.szewczyk@gmail.com>
2019-01-26 17:08:06 +0100

    Add category entity and entpoint

--- src/main/java/com/srprargh/srprargh/SrprarghApplication.java
+++ src/main/java/com/srprargh/srprargh/SrprarghApplication.java
@@ -1,23 +1,12 @@
 package com.srprargh.srprargh;
 
-import com.srprargh.srprargh.model.User;
-import com.srprargh.srprargh.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
-import java.util.Collection;
 
 @SpringBootApplication
 public class SrprarghApplication {
-
-	private final UserRepository userRepository;
-
-	@Autowired
-	public SrprarghApplication(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SrprarghApplication.class, args);
@@ -26,12 +15,6 @@ public class SrprarghApplication {
 	@PostConstruct
 	public void initApplication() {
 
-		User marcin = new User("Marcin");
-
-		userRepository.save(marcin);
-		System.out.println("Siema");
-		Collection<User> all = userRepository.findAll();
-		System.out.println(all.size());
 	}
 
 }
