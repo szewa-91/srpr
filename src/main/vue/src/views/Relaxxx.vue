e12f65b805fc387e38ac726b38e2a8911740fd1d
Andrzej Wiącek <wiacek.and@gmail.com>
2019-01-31 19:27:41 +0100

    Configure Webpack Dev Server proxy

--- src/main/vue/src/views/Relaxxx.vue
+++ src/main/vue/src/views/Relaxxx.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="relax">
     <h1>Tutaj piloci będą się relaksować. Arghhh!!!</h1>
+    <button v-on:click="fecz">Fecz</button>
     <RelaxContent msg="Wybierz dźwięk" />
   </div>
 </template>
@@ -14,5 +15,17 @@ import RelaxContent from '@/components/RelaxContent.vue'; // @ is an alias to /s
     RelaxContent,
   },
 })
-export default class Relaxxx extends Vue {}
+export default class Relaxxx extends Vue {
+
+  public fecz() {
+    console.log('fetching...');
+    fetch('/api/categories')
+    .then(res => res.json())
+    .then((res) => {
+      console.log(res);
+    });
+  }
+
+
+}
 </script>
