fe29d1a72fa279fec94c15da02b11f92f68ff4f1
Szewa <marcin91.szewczyk@gmail.com>
2019-02-14 08:43:28 +0100

    Add endpoints for sounds

--- src/main/java/com/srprargh/srprargh/rest/CategoryRestService.java
+++ src/main/java/com/srprargh/srprargh/rest/CategoryRestService.java
@@ -1,10 +1,14 @@
 package com.srprargh.srprargh.rest;
 
+import java.util.Optional;
 import com.srprargh.srprargh.model.Category;
+import com.srprargh.srprargh.model.Sound;
 import com.srprargh.srprargh.repositories.CategoryRepository;
+import com.srprargh.srprargh.repositories.SoundRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +18,21 @@ import java.util.Collection;
 @RequestMapping("/categories")
 public class CategoryRestService {
     private CategoryRepository categoryRepository;
+    private SoundRepository soundRepository;
 
     @Autowired
-    public CategoryRestService(CategoryRepository categoryRepository) {
+    public CategoryRestService(CategoryRepository categoryRepository,SoundRepository soundRepository) {
         this.categoryRepository = categoryRepository;
+        this.soundRepository = soundRepository;
     }
 
     @GetMapping("/")
     public ResponseEntity<Collection<Category>> getAllCategories() {
         return ResponseEntity.ok(categoryRepository.findAll());
+    }
+
+    @GetMapping("/{id}/sounds")
+    public ResponseEntity<Collection> getSoundsByCategory(@PathVariable Long id) {
+        return ResponseEntity.ok(soundRepository.findByCategory(categoryRepository.findById(id).get()));
     }
 }
