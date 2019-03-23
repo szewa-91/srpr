f5166a27eb1b19607967f43c83dbedb1b481e72a
Szewa <marcin91.szewczyk@gmail.com>
2019-03-23 12:00:45 +0100

    Quick fix of unused import

--- src/main/java/com/srprargh/srprargh/SrprarghApplication.java
+++ src/main/java/com/srprargh/srprargh/SrprarghApplication.java
@@ -3,13 +3,11 @@ package com.srprargh.srprargh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
 public class SrprarghApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SrprarghApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SrprarghApplication.class, args);
+    }
 }
 
